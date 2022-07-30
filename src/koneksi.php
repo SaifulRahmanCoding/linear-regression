c497946edb83ca8f263fb0c01225c0fcf518b317
Saiful Rahman <sr.perante@gmail.com>
2022-07-31 00:03:53 +0700

    koneksi online

--- src/koneksi.php
+++ src/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // kalau pakai offline
-$db = mysqli_connect("localhost","root","","dm_lr");
+// $db = mysqli_connect("localhost","root","","dm_lr");
 
 // kalau online
-// $db = mysqli_connect("sql202.ezyro.com","ezyro_32252488","jveis0ls","ezyro_32252488_dm_lr");
+$db = mysqli_connect("sql202.ezyro.com","ezyro_32252488","jveis0ls","ezyro_32252488_dm_lr");
 
 // cek koneksi
 if (!$db) {
